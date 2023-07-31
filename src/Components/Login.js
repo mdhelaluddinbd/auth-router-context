de68c2a55140a1c1673a4d89b0ba2eddfd038222
Md.Helal Uddin <mdhelaluddin.cse@gmail.com>
2023-07-31 08:21:57 +0600

    Login and register components design  done

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,11 +1,64 @@
-import React from 'react';
+import React from "react";
+import { Form, Link } from "react-router-dom";
 
 const Login = () => {
-    return (
-        <div>
-            <h1>This is login page</h1>
+    const handleSubmit=event=>{
+        event.preventDefault();
+        const form=event.target;
+        const email=form.email.value;
+        const password=form.password.value;
+        console.log(email,password)
+
+        
+    }
+  return (
+    <div>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col ">
+          <div className="text-center lg:text-left">
+            <h1 className="text-5xl font-bold">Please Login Now!</h1>
+           
+          </div>
+          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <Form className="card-body" onSubmit={handleSubmit}>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  placeholder="Enter Your Email"
+                  className="input input-bordered"
+                  name="email"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  placeholder="Enter Your Password"
+                  className="input input-bordered"
+                  name="password"
+                  required
+                />
+                <label className="label">
+                  <Link href="#" className="label-text-alt link link-hover">
+                    Forgot password?
+                  </Link>
+                </label>
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-primary">Login</button>
+              </div>
+            </Form>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Login;
