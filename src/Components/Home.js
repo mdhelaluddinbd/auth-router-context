10d284f922b86334929cd1fe89d5078c10b62526
Md.Helal Uddin <mdhelaluddin.cse@gmail.com>
2023-07-31 23:20:53 +0600

    Update the home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
     const {user}=useContext(AuthContext);
     return (
         <div>
-            <h1 className='text-2xl mt-5 font-bold'>Hi! {user?.displayName}</h1>
+            <h1 className='text-2xl mt-5 font-bold'>Welcome to our web site {user?.displayName}</h1>
         </div>
     );
 };
