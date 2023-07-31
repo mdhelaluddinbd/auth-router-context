0ac4aab74c571132d783231503f711d9a90d9710
Md.Helal Uddin <mdhelaluddin.cse@gmail.com>
2023-07-31 07:41:11 +0600

    Home ,Login and Register router configuration  done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import './App.css';
+import Main from './Components/Layout/Main';
+import Home from './Components/Home';
+import Login from './Components/Login';
+import Register from './Components/Register';
 
 function App() {
+  const router=createBrowserRouter([
+    {
+      path:"/",
+      element:<Main></Main>,
+      children:[
+        {
+          path:"/",
+          element:<Home></Home>
+        },
+        {
+          path:"/login",
+          element:<Login></Login>
+        },
+        {
+          path:"/register",
+          element:<Register></Register>
+        }
+      ]
+    }
+  ])
   return (
     <div className="App">
-    
+    <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
